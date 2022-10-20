b60383fe712a837281b76a4c6a0712013843bb63
Winter <thetruewinter@gmail.com>
2022-10-20 11:02:19 +0100

    Implements the maximum function.
    Returns the maximum value from two inputs.

--- Math.java
+++ Math.java
@@ -1,5 +1,18 @@
 public class Math {
 
 // Math functions
-
+    /*************
+     * Developer: Connor Winter
+     * University ID: 210033243
+    *************/
+    /**
+     * Returns the maximum value from two inputs.
+     * @param one the first value
+     * @param two the second value
+     * @return the maximum value of the two inputs
+     */
+    public int getMax(int one, int two) {
+        if (one >= two) return one;
+        else return two; 
+    }
 }
